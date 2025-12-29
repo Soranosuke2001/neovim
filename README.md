3692f466d4477268f6377a23e2b60cc49fc64b8b
Soranosuke2001 <sorazora1.4@gmail.com>
2025-12-29 20:55:06 +0900

    updated readme

---
+++ README.md
@@ -0,0 +1,8 @@
+# Personal NeoVim Config
+
+## Checks
+
+1. `after/plugin/java.lua`
+    - lombok path
+    - Java path
+
--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -90,6 +90,7 @@ return {
 		local ensure_installed = vim.tbl_keys(servers or {})
 		vim.list_extend(ensure_installed, {
 			"stylua",
+			"jdtls",
 		})
 
 		require("mason-tool-installer").setup({
